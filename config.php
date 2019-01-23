36bbfbc3e595d0f94d3b375bb5e4808c012be3e7
Your Name <you@example.com>
2019-01-23 19:41:14 +0100

    first commit

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Enable debug mode
+ */
+const DEBUG = true;
+
+/**
+ * An array of Alexa App IDs that are authorized to use this endpoint
+ */
+const SUPPORTED_APPLICATION_IDS = array(
+    'amzn1.ask.skill.c4420337-76e8-48bb-af72-6cafdb211c78'
+);
+
+/**
+ * Forces authenticator to allow any application ID
+ */
+const ALLOW_ALL_REQUESTS = true;
